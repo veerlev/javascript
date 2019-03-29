29da67e3898324e2c97f6270ec87a74ebce8b27e
veerlev <veerle.verkeerd@gmail.com>
2019-03-29 22:37:37 +0200

    Replace script file with facebook.js

---
+++ facebook.js
@@ -0,0 +1,47 @@
+var username = {
+	username: "anUser",
+	password: "noneofyourbusiness",
+};
+
+var database = [username];
+var newsfeed = [
+	{
+		username: "anotherUser",
+		timeline: [
+		{
+			postId: 1,
+			post: "ASDF",
+		},
+		{
+			postId: 2,
+			post: "QWER",
+		},
+		{
+			postId: 3,
+			post: "poiu",
+		}
+		],
+	},
+	{
+username: "yetAnotherUser",
+		timeline: [
+		{
+			postId: 1,
+			post: "dfsds",
+		},
+		{
+			postId: 2,
+			post: "QWEsdffdR",
+		},
+		],
+	},
+	{
+		username: "lastUser",
+		timeline: [
+		{
+			postId: 1,
+			post: "kjnhkjkj",
+		}
+		],
+	}
+];
