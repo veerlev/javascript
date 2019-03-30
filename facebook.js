fe1acf2b50a116952df1f895876d749af27e68ce
veerlev <veerle.verkeerd@gmail.com>
2019-03-30 15:53:07 +0200

    Add login function

--- facebook.js
+++ facebook.js
@@ -1,6 +1,6 @@
 var username = {
-	username: "anUser",
-	password: "noneofyourbusiness",
+	username: "george",
+	password: "georgehasapass",
 };
 
 var database = [username];
@@ -8,40 +8,57 @@ var newsfeed = [
 	{
 		username: "anotherUser",
 		timeline: [
-		{
-			postId: 1,
-			post: "ASDF",
-		},
-		{
-			postId: 2,
-			post: "QWER",
-		},
-		{
-			postId: 3,
-			post: "poiu",
-		}
+			{
+				postId: 1,
+				post: "ASDF",
+			},
+			{
+				postId: 2,
+				post: "QWER",
+			},
+			{
+				postId: 3,
+				post: "poiu",
+			}
 		],
 	},
 	{
-username: "yetAnotherUser",
-		timeline: [
-		{
-			postId: 1,
-			post: "dfsds",
-		},
-		{
-			postId: 2,
-			post: "QWEsdffdR",
-		},
-		],
+		username: "yetAnotherUser",
+			timeline: [
+				{
+					postId: 1,
+					post: "dfsds",
+				},
+				{
+					postId: 2,
+					post: "QWEsdffdR",
+				},
+			],
 	},
 	{
 		username: "lastUser",
-		timeline: [
-		{
-			postId: 1,
-			post: "kjnhkjkj",
-		}
+			timeline: [
+			{
+				postId: 1,
+				post: "kjnhkjkj",
+			}
 		],
 	}
 ];
+
+var user = prompt("Username: ");
+var pass = prompt("Password: ");
+
+function login(anUser, aPass){
+	if ((anUser === username.username) && (aPass === username.password)){	
+		console.log ("Hi, " + anUser + "!\n");
+		for (var i = 0; i < newsfeed.length; i++){
+			console.log(newsfeed[i].timeline[0].post + "\n");
+		}	
+	}
+	else {
+		console.log("Username or password incorrect!");
+	}
+}
+
+login(user, pass);
