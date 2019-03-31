b23285055b79e587e574df4fa2453c715882fd70
veerlev <veerle.verkeerd@gmail.com>
2019-03-31 21:23:19 +0300

    Add style

--- facebook.js
+++ facebook.js
@@ -1,3 +1,21 @@
+var css = document.querySelector("h1");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient(){
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value
+	 + ", " 
+	 + color2.value 
+	 + ")";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
 var button = document.getElementsByTagName("button")[0];
 
 var database = [
