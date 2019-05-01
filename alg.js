936e6c1af4c9399b3db15df9bd5f82335b16bea7
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-01 18:50:39 +0300

    Add findElement function

--- alg.js
+++ alg.js
@@ -87,3 +87,14 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+function findElement(arr, func) {
+  for (let item of arr) {
+    if (func(item)){
+      return item;
+    }
+  }
+  return undefined;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
