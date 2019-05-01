c270ff7d6d2532452f6312c9e6df5b1f09279be7
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-01 18:40:43 +0300

    Update alg.js

--- alg.js
+++ alg.js
@@ -79,3 +79,11 @@ function repeatStringNumTimes(str, num) {
 }
 
 repeatStringNumTimes("abc", 3);
+function truncateString(str, num) {
+  if (str.length > num){
+    str = str.slice(0, num) + "...";
+  }
+  return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
