96bb14d3d809569dd7485e8cd835fb35129268c8
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-01 18:29:05 +0300

    Add confirmEnding function

--- alg.js
+++ alg.js
@@ -55,3 +55,11 @@ function largestOfFour(arr) {
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+function confirmEnding(str, target) {
+  let regex = new RegExp(target + "$");
+
+  return regex.test(str);
+}
+
+confirmEnding("Bastian", "n");
