ccd36ce84017b0ab9079c97ec7de72e06dc37ba7
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-03 17:56:41 +0300

    Add bookList challenge

--- functional.js
+++ functional.js
@@ -7,3 +7,37 @@ function incrementer (value) {
 
 var newValue = incrementer(fixedValue); // Should equal 5
 console.log(fixedValue);
+
+
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function add (bookName) {
+  
+  return bookList.push(bookName);
+  
+  // Add your code above this line
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function remove (bookName) {
+  if (bookList.indexOf(bookName) >= 0) {
+    
+    return bookList.splice(0, 1, bookName);
+    
+    // Add your code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
