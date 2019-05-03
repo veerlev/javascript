d06d496c85a66b8750c297329e0955b574901bc2
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-03 18:32:55 +0300

    Add own filter

--- functional.js
+++ functional.js
@@ -186,3 +186,23 @@ Array.prototype.myMap = function(callback){
 var new_s = s.myMap(function(item){
   return item * 2;
 });
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback){
+  var newArray = [];
+  // Add your code below this line
+  for (let i = 0; i < s.length; i++){
+    if (callback(s[i])){
+      newArray.push(s[i]);
+    }
+  }
+  // Add your code above this line
+  return newArray;
+
+};
+
+var new_s = s.myFilter(function(item){
+  return item % 2 === 1;
+});
