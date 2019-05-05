c200bf1e5f26e3dc36a7c2b20c10e19ecc069fed
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-05 11:04:45 +0300

    Add use of 'some' function

--- functional.js
+++ functional.js
@@ -260,3 +260,11 @@ function checkPositive(arr) {
   // Add your code above this line
 }
 checkPositive([1, 2, 3, -4, 5]);
+
+function checkPositive(arr) {
+  // Add your code below this line
+  return arr.some(x => x >= 0);
+  
+  // Add your code above this line
+}
+checkPositive([1, 2, 3, -4, 5]);
