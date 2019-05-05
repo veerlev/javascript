30f92d6ccc91670b50f82d120f26e3128a8b3bff
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-05 22:36:21 +0300

    Add sum of primes function

--- functional.js
+++ functional.js
@@ -476,3 +476,24 @@ function sumFibs(num) {
 }
 
 sumFibs(4);
+
+function isPrime(num){
+  for (let i = 2; i <= Math.sqrt(num); i++){
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function sumPrimes(num) {
+  let sum = 0;
+  for (let i = 2; i <= num; i++){
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+sumPrimes(10);
