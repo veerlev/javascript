80a9551cebf2eb3a933cf3dda1aeaa0fde974ea7
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-05 11:12:35 +0300

    Add curried add function

--- functional.js
+++ functional.js
@@ -268,3 +268,15 @@ function checkPositive(arr) {
   // Add your code above this line
 }
 checkPositive([1, 2, 3, -4, 5]);
+
+function add(x) {
+  // Add your code below this line
+  return function(y) {
+    return function(z){
+      return x + y + z;
+    }
+  }
+  
+  // Add your code above this line
+}
+add(10)(20)(30);
