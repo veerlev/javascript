be6667b222b003eb98d8bcc4725fee0b36e6c921
veerlev <47982314+veerlev@users.noreply.github.com>
2019-05-06 11:59:38 +0300

    Add truthCheck function

--- someMoreJs.js
+++ someMoreJs.js
@@ -583,3 +583,19 @@ function binaryAgent(str) {
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+
+function truthCheck(collection, pre) {
+  // Is everyone being true?=
+  for (let item of collection){
+    if (!item.hasOwnProperty(pre)){
+      return false;
+    }
+    if (!item[pre]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
